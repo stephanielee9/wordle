4ce58555b48b0cc6eb29559bb086453d240adaf2
stephanielee9 <stephanielee@uchicago.edu>
2022-06-16 14:55:59 -0500

    coloring works for formatting guesses

--- pywordle/pywordle.py
+++ pywordle/pywordle.py
@@ -14,7 +14,7 @@ class color:
    YELLOW = '\033[93m'
    RED = '\033[91m'
    BOLD = '\033[1m'
-   GRAY = '\x1b[37m'
+   GREY = '\x1b[37m'
    UNDERLINE = '\033[4m'
    END = '\033[0m'
 
@@ -38,23 +38,41 @@ def print_letters(letter_list):
 
     pass
 
-def validate_guess():
-    pass
+def validate_guess(guess, words_list):
+    
+    checks = 0
+    if guess.isalpha():
+        checks += 1
+    if len(guess) == 5:
+        checks += 1
+    if guess.upper() in words_list:
+        checks += 1
+    
+    if checks == 3:
+        return True
+    else:
+        return False
 
-def format_guess(guess):
+
+def format_guess(guess, word):
     '''
     formats input
+    adds spaces between letters and capitalizes letters
+
+    if letter is in correct position, green
+    if letter is in word but not correct position, yellow
+    else, grey
     '''
 
-    word = ''
-    for letter in guess:
-        word += f'{letter.upper()} '
-    return(word)
-
-
-
-
-
+    formatted_guess = ''
+    for i in range(5):
+        if guess[i].upper() == word[i]:
+            formatted_guess += color.GREEN + color.BOLD + guess[i].upper() + color.END + " "
+        elif guess[i].upper() in word:
+            formatted_guess += color.YELLOW + color.BOLD + guess[i].upper() + color.END + " "
+        else:
+            formatted_guess += color.GREY + color.BOLD + guess[i].upper() + color.END + " "
+    return(formatted_guess)
 
 def main():
 
@@ -68,10 +86,11 @@ def main():
     print_guesses(guesses)
 
     for _ in range(6):
-        guess = input(color.GRAY + color.BOLD + '>>> ' + color.END)
-        while not (guess.isalpha() and len(guess) == 5):
-            guess = input(color.GRAY + color.BOLD + '>>> ' + color.END)
-        guesses.append(format_guess(guess))
+        guess = input(color.PURPLE + color.BOLD + '>>> ' + color.END)
+        # while not (guess.isalpha() and len(guess) == 5):
+        while not validate_guess(guess, words_list):
+            guess = input(color.PURPLE + color.BOLD + '>>> ' + color.END)
+        guesses.append(format_guess(guess, word))
         print_guesses(guesses)
 
 
